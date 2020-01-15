464f0f3af12ee857bc0d7d555ebb13bdb007d2d7
Dsharma624 <devisharma17817@gmail.com>
2020-01-15 11:11:45 -0500

    third commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,6 +14,15 @@ public class Student {
     private String name;
     private double GPA;
     private String email;
+    private int Phone;
+
+    public void setPhone(int Phone) {
+        this.Phone = Phone;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
 
     public String getEmail() {
         return email;
