16d7f119aedb5dd691607ddaef90dd3b24aed835
Dsharma624 <devisharma17817@gmail.com>
2020-01-22 09:52:08 -0500

    part time commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,6 +13,7 @@ public class Student {
     
     private String name;
     private double GPA;
+    private String status;
     private String email;
     public String getEmail() {
         return email;
@@ -46,6 +47,20 @@ public class Student {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
     }
 
 }
